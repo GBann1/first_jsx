d172576c0bb9d3a2a05df0dbe147c76f9ce4c083
GBann <GTBannon@protonmail.com>
2023-08-04 16:37:46 -0600

    added jsx files that should be ignored 👍

---
+++ src/components/h1Header.jsx
@@ -0,0 +1,3 @@
+const Header = () => {
+    return React.createElement("h1", {}, "Hello Dojo!")
+}
---
+++ src/components/h3Title.jsx
@@ -0,0 +1,3 @@
+const Title = () => {
+    return React.createElement("h3",{}, "Things I need to do:")
+}
